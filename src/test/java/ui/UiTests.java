5d05ec5a5c9275823cb0603d206e12646fe25bd2
alexkokhanchuk <alex.kokhanchuk@observepoint.com>
2022-01-21 18:17:22 +0200

    add driver manager

--- src/test/java/ui/UiTests.java
+++ src/test/java/ui/UiTests.java
@@ -4,11 +4,17 @@ import managers.LocalDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 import pages.VehicleFinder;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class UiTests {
 
@@ -29,6 +35,35 @@ public class UiTests {
         LocalDriverManager.removeDriver();
     }
 
+
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = {};
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            int sum = 0;
+
+            for (int a = i + 1; i < nums.length; i++) {
+                sum = nums[i] + nums[a];
+
+                if (sum == target) {
+
+                    return new int[]{nums[i], nums[a]};
+                }
+            }
+
+
+        }
+        return result;
+    }
+
+    @Test
+    public void chechSum() {
+        int target = 9;
+        int[] nums = {2, 7, 11, 15};
+
+
+    }
+
     @Test
     public void openCopart() {
         logger.info("Starting test ");
@@ -37,5 +72,29 @@ public class UiTests {
                 .selectMake("Acura")
                 .selectYears("2015", "2019");
 
+    }
+
+    @Test
+    public void luxTests() {
+        final ArrayList<Integer> objects = new ArrayList<>();
+        objects.add(1);
+        objects.add(1);
+        objects.add(2);
+        objects.add(3);
+
+        final Set<Integer> collect = new HashSet<>(objects);
+
+        System.out.println(collect);
+    }
+
+    public boolean isSimple(int number) {
+        boolean isSimple = true;
+
+        for (int i = 2; i < number; i++) {
+            double result = number % i;
+            if (result == 0) isSimple = false;
+        }
+        
+        return isSimple;
     }
 }
